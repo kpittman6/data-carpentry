905edf8f064e05f726a9f2d327a581000fcc40cc
kpittman6 <kyled.pittman6@gmail.com>
2021-03-04 15:17:52 -0600

    Update from Lesson 7, vectoriazation

--- plot_precipitation_climatology.py
+++ plot_precipitation_climatology.py
@@ -51,6 +51,24 @@ def create_plot(clim, model, season, gridlines=False):
     title = f'{model} precipitation climatology ({season})'
     plt.title(title)
 
+def apply_mask(darray, sftlf_file, realm):
+    """Mask ocean or land using a sftlf (land surface fraction) file.
+
+    Args:
+     darray (xarray.DataArray): Data to be masked
+     sftlf_file (str): Land surface fraction file
+     realm (str): Realm to mask
+
+    """
+    dset = xr.open_dataset(sftlf_file)
+    if realm == 'land':
+        masked_darray = darray.where(dset['sftlf'].data < 50)
+    else:
+        masked_darray = darray.where(dset['sftlf'].data >50)
+
+    return masked_darray
+
+
 
 def main(inargs):
     """Run the program."""
@@ -60,8 +78,13 @@ def main(inargs):
     clim = dset['pr'].groupby('time.season').mean('time', keep_attrs=True)
     clim = convert_pr_units(clim)
 
+    if inargs.mask:
+        sftlf_file, realm = inargs.mask
+        clim = apply_mask(clim, sftlf_file, realm)
+
     create_plot(clim, dset.attrs['source_id'], inargs.season, gridlines=inargs.gridlines, levels=inargs.cbar_levels)
     plt.savefig(inargs.output_file, dpi=200)
+
 
 
 if __name__ == '__main__':
@@ -74,6 +97,7 @@ if __name__ == '__main__':
     
     parser.add_argument("--gridlines", action="store_true", default=False, help="Include gridlines on the plot")
     parser.add_argument("--cbar_levels", type=float, nargs='*', default=None, help='list of levels / tick marks to appear on the colorbar')
+    parser.add_argument("--mask", type=str, nargs=2, metavar=('SFTLF_FILE', 'REALM'), default=None, help="""Provide sftlf file and realm to maks ('land' or 'ocean')""")
 
     args = parser.parse_args()
     
